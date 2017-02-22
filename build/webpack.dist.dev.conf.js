67c21abf2bf02aac3e666f3feb24de596350fddc
Jerrychen <Jerrychen@uniweibo.com>
2017-02-22 19:11:59 +0800

    created button

---
+++ build/webpack.dist.dev.conf.js
@@ -0,0 +1,76 @@
+var path = require('path');
+var webpack = require('webpack');
+var utils = require('./utils');
+
+function resolve(dir) {
+    return path.join(__dirname, '..', dir)
+}
+
+module.exports = {
+    entry: {
+        main: './src/index.js'
+    },
+    output: {
+        path: path.resolve(__dirname, '../dist'),
+        publicPath: '/dist/',
+        filename: 'uni-component.js',
+        library: 'uni-component',
+        libraryTarget: 'umd',
+        umdNamedDefine: true
+    },
+    externals: {
+        vue: {
+            root: 'Vue',
+            commonjs: 'vue',
+            commonjs2: 'vue',
+            amd: 'vue'
+        }
+    },
+    resolve: {
+        extensions: ['.js', '.vue', '.json'],
+        modules: [
+            resolve('src'),
+            resolve('node_modules')
+        ],
+        alias: {
+            'vue$': 'vue/dist/vue.common.js',
+            'src': resolve('src'),
+            'assets': resolve('src/assets'),
+            'components': resolve('src/components')
+        }
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            }, {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                include: [resolve('src')]
+            }, {
+                test: /\.css$/,
+                loader: 'style!css!autoprefixer'
+            }, {
+                test: /\.scss$/,
+                loader: 'style!css!scss'
+            }, {
+                test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+                loader: 'url-loader',
+                query: {
+                    limit: 10000,
+                    name: utils.assetsPath('img/[name].[hash:7].[ext]')
+                }
+            }, {
+                test: /\.(html|tpl)$/,
+                loader: 'vue-html'
+            }]
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: '"development"'
+            }
+        })
+    ]
+}
